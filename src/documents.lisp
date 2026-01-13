9a1787eab4e769a7b21dc3dd2c3ff9222ff92403
N545PY <nsaspy@fedora.email>
2026-01-13 18:18:39 -0500

    bump version

--- src/documents.lisp
+++ src/documents.lisp
@@ -1,6 +1,6 @@
 (in-package :starintel)
 
-(defparameter +starintel-doc-version+ "0.7.3")
+(defparameter +starintel-doc-version+ "0.8.0")
 (defparameter *default-hash-algo* :md5)
 
 (defun unix-now ()
