e36e3d1f79494bafe5a795c7750ef6a75e479b73
N545PY <nsaspy@fedora.email>
2026-01-17 16:59:23 -0500

    support predicates
    eg owns, bought, child, parent.

--- src/relations.lisp
+++ src/relations.lisp
@@ -3,13 +3,14 @@
 (defclass relation (document)
   ((source :accessor relation-source :type string :initarg :source :initform "")
    (target :accessor relation-target :type string :initarg :target :initform "")
+   (predicate :accessor relation-predicate :type string :initarg :predicate :initform "")
    (note :accessor relation-note :type string :initarg :note :initform "")))
 
 (defmethod set-id ((doc relation))
   (ulid-id doc))
 
 
-(defun new-relation (dataset source target note)
+(defun new-relation (dataset source target &key note  predicate)
   "Create a New Booker Relation"
   (let ((relation (make-instance 'relation :dataset dataset :source source :target target :note note)))
     (set-meta relation dataset)
