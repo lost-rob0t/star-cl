fe7f0877c132bdd95217cc20c0a10b531a440b05
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-07-26 19:40:53 -0400

    export spec packages through reusable Nix flake

--- flake.nix
+++ flake.nix
@@ -1,143 +1,229 @@
 {
   description = "Star-cl: StarIntel v0.9.0 document implementation";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
-      sbcl' = pkgs.sbcl.withOverrides (self: super: {
-        cms-ulid = pkgs.sbcl.buildASDFSystem rec {
-          pname = "cms-ulid";
-          version = "latest";
-          src = pkgs.fetchgit {
-            url = "https://gitlab.com/colinstrickland/cms-ulid.git";
-            rev = "fff84302dee5db42fb90aafd834af3ffbfd6c2bb";
-            hash = "sha256-B5rekME60bWHk47kDepQpOr9drjgXjZBiRpA+Ob1CuU=";
-          };
+      forAllSystems = nixpkgs.lib.genAttrs systems;
 
-          lispLibs = [
-            self.local-time
-            self.ironclad
-            self.bit-smasher
-            self.serapeum
-          ];
+      mkPackageSet = pkgs:
+        let
+          sbcl' = pkgs.sbcl.withOverrides (lispFinal: _lispPrev: {
+            cms-ulid = pkgs.sbcl.buildASDFSystem {
+              pname = "cms-ulid";
+              version = "0-unstable-2024-04-20";
 
-          dontStrip = true;
+              src = pkgs.fetchgit {
+                url = "https://gitlab.com/colinstrickland/cms-ulid.git";
+                rev = "fff84302dee5db42fb90aafd834af3ffbfd6c2bb";
+                hash = "sha256-B5rekME60bWHk47kDepQpOr9drjgXjZBiRpA+Ob1CuU=";
+              };
+
+              lispLibs = [
+                lispFinal.local-time
+                lispFinal.ironclad
+                lispFinal.bit-smasher
+                lispFinal.serapeum
+              ];
+
+              dontStrip = true;
+            };
+
+            starintel-v090 = pkgs.sbcl.buildASDFSystem {
+              pname = "starintel-v090";
+              version = "0.9.0";
+              src = self;
+
+              lispLibs = [
+                lispFinal.jzon
+                lispFinal.cl-ppcre
+              ];
+
+              systems = [ "starintel-v090" ];
+              asdFilesToKeep = [ "starintel-v090.asd" ];
+              dontStrip = true;
+            };
+
+            starintel = pkgs.sbcl.buildASDFSystem {
+              pname = "starintel";
+              version = "0.9.0";
+              src = self;
+
+              lispLibs = [
+                lispFinal.jsown
+                lispFinal.jzon
+                lispFinal.cl-ppcre
+                lispFinal.ironclad
+                lispFinal.local-time
+                lispFinal.cms-ulid
+                lispFinal.str
+                lispFinal.closer-mop
+              ];
+
+              systems = [ "starintel" ];
+              asdFilesToKeep = [
+                "src/starintel.asd"
+                "starintel-v090.asd"
+                "starintel-test.asd"
+              ];
+              dontStrip = true;
+            };
+
+            starintel-test = pkgs.sbcl.buildASDFSystem {
+              pname = "starintel-test";
+              version = "0.9.0";
+              src = self;
+
+              lispLibs = [
+                lispFinal.starintel
+                lispFinal.fiveam
+              ];
+
+              systems = [ "starintel-test" ];
+              dontStrip = true;
+            };
+          });
+
+          cms-ulid = sbcl'.pkgs.cms-ulid;
+          starintel-v090 = sbcl'.pkgs.starintel-v090;
+          starintel = sbcl'.pkgs.starintel;
+          starintel-test = sbcl'.pkgs.starintel-test;
+
+          sbcl-wrapped = sbcl'.withPackages (ps: [
+            ps.starintel
+            ps.starintel-v090
+          ]);
+
+          sbcl-test-wrapped = sbcl'.withPackages (ps: [
+            ps.starintel-test
+          ]);
+        in
+        {
+          inherit
+            cms-ulid
+            starintel-v090
+            starintel
+            starintel-test
+            sbcl-wrapped
+            sbcl-test-wrapped
+            ;
         };
-
-        starintel = pkgs.sbcl.buildASDFSystem rec {
-          pname = "starintel";
-          version = "0.9.0";
-          src = ./.;
-
-          lispLibs = [
-            self.jsown
-            self.jzon
-            self.cl-ppcre
-            self.ironclad
-            self.local-time
-            self.cms-ulid
-            self.str
-            self.closer-mop
-          ];
-
-          systems = [ "starintel" ];
-          asdFilesToKeep = [ "src/starintel.asd" "starintel-v090.asd" "starintel-test.asd" ];
-          dontStrip = true;
-        };
-
-        starintel-test = pkgs.sbcl.buildASDFSystem rec {
-          pname = "starintel-test";
-          version = "0.9.0";
-          src = ./.;
-
-          lispLibs = [
-            self.starintel
-            self.fiveam
-          ];
-
-          systems = [ "starintel-test" ];
-          dontStrip = true;
-        };
-      });
-
-      starintel = sbcl'.pkgs.starintel;
-      starintel-test = sbcl'.pkgs.starintel-test;
-      cms-ulid = sbcl'.pkgs.cms-ulid;
-
-      sbcl-wrapped = sbcl'.withPackages (ps: [
-        ps.starintel
-      ]);
-
-      sbcl-test-wrapped = sbcl'.withPackages (ps: [
-        ps.starintel-test
-      ]);
     in
     {
-      packages.${system} = {
-        default = starintel;
-        starintel = starintel;
-        starintel-test = starintel-test;
-        cms-ulid = cms-ulid;
-        sbcl-wrapped = sbcl-wrapped;
-        sbcl-test-wrapped = sbcl-test-wrapped;
-      };
+      packages = forAllSystems (system:
+        let
+          packageSet = mkPackageSet nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = packageSet.starintel;
+          inherit (packageSet)
+            cms-ulid
+            starintel-v090
+            starintel
+            starintel-test
+            sbcl-wrapped
+            sbcl-test-wrapped
+            ;
+        });
 
-      checks.${system} = {
-        starintel-tests = pkgs.stdenv.mkDerivation {
-          name = "starintel-tests-check";
-          src = ./.;
-          nativeBuildInputs = [ sbcl-test-wrapped ];
+      checks = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          packageSet = mkPackageSet pkgs;
 
-          buildPhase = ''
-            export HOME=$TMPDIR
-            export XDG_CACHE_HOME="$HOME/.cache"
+          testCheck = pkgs.stdenvNoCC.mkDerivation {
+            pname = "starintel-tests-check";
+            version = "0.9.0";
+            src = self;
 
-            cp -r $src $TMPDIR/source
-            chmod -R u+w $TMPDIR/source
-            cd $TMPDIR/source
+            nativeBuildInputs = [ packageSet.sbcl-test-wrapped ];
+            dontConfigure = true;
 
-            ${sbcl-test-wrapped}/bin/sbcl --non-interactive --no-userinit --no-sysinit \
-              --eval "(require :asdf)" \
-              --eval "(push (truename \".\") asdf:*central-registry*)" \
-              --eval "(asdf:load-system :starintel-test)" \
-              --eval "(handler-case
-                        (progn
-                          (asdf:test-system :starintel-test)
-                          (uiop:quit 0))
-                        (error (e)
-                          (format t \"~%Test error: ~a~%\" e)
-                          (uiop:quit 1)))" \
-              2>&1 | tee $TMPDIR/test-output.log
+            buildPhase = ''
+              runHook preBuild
 
-            TEST_EXIT_CODE=''${PIPESTATUS[0]}
-            if [ $TEST_EXIT_CODE -ne 0 ]; then
-              exit $TEST_EXIT_CODE
-            fi
-          '';
+              export HOME="$TMPDIR"
+              export XDG_CACHE_HOME="$HOME/.cache"
 
-          installPhase = ''
-            mkdir -p $out
-            cp $TMPDIR/test-output.log $out/test-results.log
-          '';
+              cp -r "$src" "$TMPDIR/source"
+              chmod -R u+w "$TMPDIR/source"
+              cd "$TMPDIR/source"
+
+              ${packageSet.sbcl-test-wrapped}/bin/sbcl \
+                --non-interactive \
+                --no-userinit \
+                --no-sysinit \
+                --eval "(require :asdf)" \
+                --eval "(push (truename \".\") asdf:*central-registry*)" \
+                --eval "(asdf:load-system :starintel-test)" \
+                --eval "(handler-case
+                          (progn
+                            (asdf:test-system :starintel-test)
+                            (uiop:quit 0))
+                          (error (e)
+                            (format t \"~%Test error: ~a~%\" e)
+                            (uiop:quit 1)))" \
+                2>&1 | tee "$TMPDIR/test-output.log"
+
+              test_exit_code="''${PIPESTATUS[0]}"
+              if [ "$test_exit_code" -ne 0 ]; then
+                exit "$test_exit_code"
+              fi
+
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+
+              mkdir -p "$out"
+              cp "$TMPDIR/test-output.log" "$out/test-results.log"
+
+              runHook postInstall
+            '';
+          };
+        in
+        {
+          default = testCheck;
+          starintel-tests = testCheck;
+        });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          packageSet = mkPackageSet pkgs;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              packageSet.sbcl-test-wrapped
+              pkgs.pkg-config
+            ];
+
+            shellHook = ''
+              echo "StarIntel v0.9.0 development environment ready"
+              echo "Test with: nix flake check"
+            '';
+          };
+        });
+
+      overlays.default = final: _prev:
+        let
+          packageSet = mkPackageSet final;
+        in
+        {
+          inherit (packageSet)
+            cms-ulid
+            starintel-v090
+            starintel
+            starintel-test
+            ;
         };
-      };
-
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          sbcl-test-wrapped
-          pkgs.pkg-config
-        ];
-
-        shellHook = ''
-          echo "StarIntel v0.9.0 development environment ready"
-          echo "Test with: nix flake check"
-        '';
-      };
     };
 }
